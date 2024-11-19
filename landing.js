90d14dab71a8f528fc8c8df019e66649435ec872
mayankmehar9128 <150352254+mayankmehar9128@users.noreply.github.com>
2024-11-19 14:10:28 +0530

    Update landing.js

--- landing.js
+++ landing.js
@@ -1,5 +1,15 @@
 // Slick slider init
 
+function toggleMenu() {
+  const sideNav = document.getElementById("sideNav");
+  if (sideNav.style.width === "250px") {
+      sideNav.style.width = "0";
+  } else {
+      sideNav.style.width = "250px";
+  }
+}
+
+
 $('.slider').slick({
     arrows: true,
     dots: false,
@@ -18,4 +28,35 @@ $('.slider').slick({
     .on('afterChange', function(){
       $('.slick-list').removeClass('do-transition')
     });
+     
+
+
+
+
+
+
+
+
+
+
+// Slick slider init
+
+// $('.slider').slick({
+//     arrows: true,
+//     dots: false,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 1,
+//     centerMode: true,
+//     variableWidth: true,
+//     draggable: false
+//   });
+  
+//   $('.slider')
+//     .on('beforeChange', function(event, slick, currentSlide, nextSlide){
+//       $('.slick-list').addClass('do-transition')
+//     })
+//     .on('afterChange', function(){
+//       $('.slick-list').removeClass('do-transition')
+//     });
      
